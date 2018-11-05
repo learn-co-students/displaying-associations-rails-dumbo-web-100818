ec7a4895081c13859d8d57e9e42bce028de65fad
said shah <saidmysr20@aol.com>
2018-11-05 18:12:00 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+clickbait = Category.create!(name: "Motivation")
+clickbait.posts.create!(title: "10 Ways You Are Already Awesome", description: "postONE")
+clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe",description: "postTWO")
+clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee", description: "postTHREE")
+
+movies = Category.create!(name: "Movies")
+movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog",description: "postFOUR")
+movies.posts.create!(title: "Top 5 Movies Featuring a Cute BEAR",description: "postFIVE")
